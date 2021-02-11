96ffe680fd7f80308d0f5d524e7bf880208d2c81
Jeffrey Ha <jha@ucsd.edu>
2021-02-11 05:19:17 -0800

    finished lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,5 +26,16 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get(`/project/${idNumber}`, function (result) {
+		var projHtml = `<a href="#" class="thumbnail">
+			<img src="${result['image']}" class="img">
+			<p>${result['title']}</p>
+			<p><small>${result['date']}</small></p>
+		</a>
+		${result['summary']}`;
+		
+		$(`#project${idNumber} .details`).html(projHtml);
+	});
+
+	console.log(`User clicked on project ${idNumber}.`);
 }
